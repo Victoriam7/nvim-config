1cbe99e126152d25561cb1c2b314979cc4d182a1
Brendan Grau <bsg8376@g.rit.edu>
2024-01-22 09:50:29 -0500

    add alignment command

--- lua/usercmds/init.lua
+++ lua/usercmds/init.lua
@@ -1,7 +1,14 @@
 vim.cmd([[
+    function! Align(start, end, char, width)
+        for l in range(a:start, a:end)
+            execute ":" . l . "norm!f" . a:char . "200i 0" . a:width . "ldt" . a:char
+        endfor
+    endfunction
+
     function! FindFiles()
         echo system("find . -type f -not -path './.git/*'")
     endfunction
 
+    command! -range -nargs=+ Align call Align(<line1>, <line2>, <f-args>)
     command! FindFiles call FindFiles()
 ]])
