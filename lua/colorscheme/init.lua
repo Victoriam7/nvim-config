e27596f930102fe0ee4e6f2674dd3e9a470a8332
Brendan Grau <bsg8376@g.rit.edu>
2024-08-19 14:54:10 -0400

    update colorscheme

--- lua/colorscheme/init.lua
+++ lua/colorscheme/init.lua
@@ -1,10 +1,20 @@
 require("tokyonight").setup({
     style = "night",
     on_colors = function(colors)
-        colors.fg_gutter = "#707cb2"
-        colors.comment = "#709db2"
-        colors.dark5 = "#709db2"
-    end
+        colors.fg_gutter = "#c2d3d7"
+        colors.comment = "#c2d3d7"
+        colors.dark5 = "#c2d3d7"
+    end,
+	transparent = true,
+	styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "transparent", -- style for sidebars, see below
+    floats = "transparent", -- style for floating windows
+  },
 })
 
 require'nvim-web-devicons'.setup{}
